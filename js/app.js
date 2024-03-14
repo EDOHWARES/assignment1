f0a30480407ea865a393f4a17efe5e3945836a4b
EDOHWARES <116767410+EDOHWARES@users.noreply.github.com>
2024-03-14 01:12:57 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,73 @@
+const makeScrollSmooth = () => {
+    const navLinks = document.querySelectorAll("nav a");
+
+    navLinks.forEach((link) => {
+        link.addEventListener('click', smoothScroll)
+    })
+}
+
+document.addEventListener("DOMContentLoaded", makeScrollSmooth);
+
+const smoothScroll = (e) => {
+    e.preventDefault();
+    el = e.target
+    const targetId = el.getAttribute('href');
+    const targetElement = document.querySelector(targetId);
+
+    if (targetElement) {
+        targetElement.scrollIntoView({behaviour: 'smooth', block: 'end'});
+    }
+}
+
+const resetLocationUrl = () => {
+    const url = window.location.href;
+    const new_url = url.replace(/#\w+\s?/g, "")
+
+    if (new_url !== url) {
+        // Redirect to the new URL only if it's different
+        window.location.href = new_url;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", resetLocationUrl);
+
+const navLinks = document.querySelectorAll('nav ul li');
+
+navLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+        navLinks.forEach((navlink) => {
+            navlink.classList.remove('clicked')
+        })
+        link.classList.add('clicked')
+    })
+})
+
+const remaining_tags = document.querySelector('section.assignment .remaining')
+const extend_rt_btn = document.querySelector('section.assignment .btn-c button')
+
+extend_rt_btn.addEventListener('click', () => {
+    remaining_tags.style.display = 'block';
+    extend_rt_btn.style.display = 'none';
+});
+
+// var swiper = new Swiper(".mySwiper", {
+//     slidesPerView: 3,
+//     spaceBetween: 30,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//   });
+
+menuBtn = document.querySelector('header .container .menu');
+mobileNav = document.querySelector('header .container .nav');
+
+closeBtn = document.querySelector('header .container .close');
+
+menuBtn.addEventListener('click', () => {
+    mobileNav.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', () => {
+    mobileNav.style.display = 'none';
+});
